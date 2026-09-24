da86755d91d3c4798f720b5bb6da2c87a22b438a
priya <priyankasatyavolu2904@gmail.com>
2026-09-24 13:05:04 +0530

    Initial messsage

---
+++ pp.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number:"))
+if(a>=35):
+    print("student is passed")
+else:
+    print("student is failed")
+ 
+ 
+a=int(input("enter the number:"))
+if(a%5==0):
+    print("it is divisible by 5")
+else:
+    print("it is not divisible by 5")
